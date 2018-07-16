8aa414af9823c8257b7ab1973327e14c3a30bedc
JosephAMumford <JosephAMumford@gmail.com>
2018-07-15 22:26:07 -0500

    Update

--- NumPy/ArrayCreation.py
+++ NumPy/ArrayCreation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import pi
 
 # Create ndarray from Python list or tuple using array()
 a = np.array([2,3,4])
@@ -29,3 +30,49 @@ d = np.array([ [1,2], [3,4] ], dtype=complex)
 
 # Print d
 print d
+
+# Create 2-dimensional array initialized with zeros
+e = np.zeros( (3,4) )
+
+# Print e
+print e
+
+# Create 2-dimensional array initialized with ones
+f = np.ones( (2,3,4), dtype=np.int16 )
+
+# Print f
+print f
+
+# Create 2-dimensional array initialized with random data
+g = np.empty( (2,3) )
+
+# Print g
+print g
+
+# Create a sequence from a to b, step c (a,b,c)
+h = np.arange( 10, 30, 5 )
+
+# Print h
+print h
+
+# Create a sequence from a to b, step c (a,b,c)
+i = np.arange( 0, 2, 0.3)
+
+# Print i
+print i
+
+#Create a sequence in linear space from a to b, number of elements c (a,b,c)
+j = np.linspace(0,2,9)
+
+# Print j
+print j
+
+# Create a sequence in linear space from a to b, number of elements c (a,b,c)
+k = np.linspace(0, 2*pi, 100)
+
+# Print k
+print k
+
+l = np.sin(k)
+
+print l
